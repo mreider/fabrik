83084b15c43771793f3eb461db26ad225267361e
Matthew Reider <matthew.reider@dynatrace.com>
2025-11-29 10:43:11 +0100

    Simulate actual DB activity before

--- apps/orders/src/main/java/com/fabrik/orders/OrderService.java
+++ apps/orders/src/main/java/com/fabrik/orders/OrderService.java
@@ -22,9 +22,14 @@ public class OrderService extends OrderServiceGrpc.OrderServiceImplBase {
     public void placeOrder(OrderRequest request, StreamObserver<OrderResponse> responseObserver) {
         if ("true".equals(System.getenv("FAILURE_MODE"))) {
             try {
+                // Actually attempt a query that will timeout or fail
+                // We use a native query to simulate a slow/locked table
+                orderRepository.findAll(); 
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
+            } catch (Exception e) {
+                // Ignore the find error, we want to throw the specific timeout exception below
             }
             throw new RuntimeException("org.springframework.dao.QueryTimeoutException: PreparedStatementCallback; SQL [INSERT INTO orders ...]; Query timeout; nested exception is org.postgresql.util.PSQLException: ERROR: canceling statement due to user request");
         }
